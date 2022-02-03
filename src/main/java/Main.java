d5c129a95ae48e794b44e743f99dd285b8911241
Sethu Budaza <sethu@terrafirma-solutions.com>
2022-02-03 19:05:56 +0200

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import com.numberRangeSummarizer.NumberRangeSummarizer;
+import com.numberRangeSummarizer.NumberSummarizerImpl;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by SethuBudaza on 2022/02/03.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        NumberRangeSummarizer summarizer = new NumberSummarizerImpl();
+
+        String input = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
+        // Result: "1, 3, 6-8, 12-15, 21-24, 31"
+
+        try {
+            Collection<Integer> collectionInput = (List<Integer>) summarizer.collect(input);
+            String range = summarizer.summarizeCollection(collectionInput);
+
+            System.out.println(range);
+        } catch (ClassCastException exc) {
+            System.out.println("Please review the string there might be a character");
+        }
+    }
+}
